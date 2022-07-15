43575131d41b43e3215ca0dd5aaaba19eb85d2cf
Andre Stackhouse <andre@starlite.Home>
2022-07-15 16:42:19 -0700

    Fix html issue, remove all caps rule (until futher clarified)

--- src/pages/about/conduct.js
+++ src/pages/about/conduct.js
@@ -13,12 +13,11 @@ const ConductPage = () =>
       <ul>
         <li>No personal attacks</li>
         <li>No yelling</li>
-        <li>No typing in ALL CAPS</li>
         <li>No hate speech or racial slurs</li>
         <li>You may not be in the group under a false identity</li>
         <li>No doxxing</li>
         <li>Conversations are considered private to the group unless consent to share outside of the community is given</li>
-        <li>Accusations of being a plant/informant/CIA/FBI/bot are not to be taken lightly, making such an accusation is considered </li>serious and making such an accusation without probable cause is not tolerated.
+        <li>Accusations of being a plant/informant/CIA/FBI/bot are not to be taken lightly, making such an accusation is considered serious and making such an accusation without probable cause is not tolerated.</li>
         <li>Debate and disagreement are encouraged</li>
         <li>It is okay to be wrong in this community</li>
         <li>It is okay for others to be wrong or have a different opinion and you may not attack them for disagreeing</li>
