06db6df8c7cf04e00bb4836142019fcf5eb1189a
Andre Stackhouse <andre@starlite.Home>
2022-07-15 14:16:10 -0700

    Fix mailto link, include contact info on 'media' page

--- src/pages/about/contact.js
+++ src/pages/about/contact.js
@@ -8,7 +8,7 @@ const ContactPage = () =>
     <Layout pageTitle="Contact" sidebarLinks={AboutSidebarLinks} activeHeaderLink="About">
     <main>
       <h1>Contact</h1>
-      <p>You can contact Medicare for All EVERYWHERE via email at <strong><a href='mailto=massmovement@gmail.com'>massmovement@gmail.com</a></strong>.</p>
+      <p>You can contact Medicare for All EVERYWHERE via email at <a href="mailto:massmovement.healthcare@gmail.com">massmovement.healthcare@gmail.com</a>.</p>
       <p>You may also join our Slack community to connect with our leadership and volunteers across the country. To get a Slack invite please view and agree to our <strong><a href='https://actionnetwork.org/forms/code-of-conduct-2?source=direct_link&'>Code of Conduct.</a></strong></p>
     </main>
     </Layout>
