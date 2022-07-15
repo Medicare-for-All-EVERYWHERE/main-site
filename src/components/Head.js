efb4c2f7532230b72a58562bd42496d63588e69f
Andre Stackhouse <andre@starlite.Home>
2022-07-15 09:26:51 -0700

    Refactor/update site metadata in Head

--- src/components/Head.js
+++ src/components/Head.js
@@ -2,12 +2,12 @@ import * as React from "react"
 import { Helmet } from 'react-helmet'
 import Screenshot from '../images/screenshot.png'
 
-const Head = ({pageTitle, siteTitle}) =>
+const Head = ({pageTitle, siteTitle = 'Medicare for All EVERYWHERE'}) =>
   <Helmet>
     <html lang="en" />
     <title>{pageTitle} | {siteTitle}</title>
     <meta content={Screenshot} property="og:image" />
-    <meta property="og:description" content="Website meta description" />
+    <meta property="og:description" content="Medicare for All EVERYWHERE is an aspiring coalition including every US state and territory united behind the goal of Medicare for All both statewide and federally." />
   </Helmet>
 
 export default Head
