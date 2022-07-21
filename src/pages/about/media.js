b6bccbbb7713b5bafb18c9a0295c2207ea15da39
Andre Stackhouse <andre@starlite.Home>
2022-07-21 16:49:11 -0700

    Update media page heading

--- src/pages/about/media.js
+++ src/pages/about/media.js
@@ -7,7 +7,7 @@ const MediaPage = () =>
   <div className='Layout'>
     <Layout pageTitle="Media" sidebarLinks={AboutSidebarLinks} activeHeaderLink="About">
     <main>
-      <h1>Press</h1>
+      <h1>Media Coverage</h1>
       <p>Medicare for All EVERYWHERE has not even existed for a year and has already been covered in publications and podcasts.</p>
       <ul>
         <li><a href='https://www.latimes.com/california/story/2022-02-25/demise-of-single-payer-healthcare-in-california-trips-up-efforts-in-other-states'>Demise of single-payer healthcare in California trips up efforts in other states</a></li>
