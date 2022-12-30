8d5804e1ba12e1a30babc331f6ed03c9fa42a3ce
Andre Stackhouse <captainstack@gmail.com>
2022-12-30 15:23:55 -0800

    Update about page content.

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -6,8 +6,11 @@ import '../../styles/styles.scss'
 const AboutPage = () =>
   <Layout pageTitle="About" sidebarLinks={AboutSidebarLinks} activeHeaderLink="About">
     <h1>About</h1>
-    <p>Medicare for All EVERYWHERE held its first meeting in October 2021 and has in its short time organized several actions across multiple states and in Washington D.C.</p>
-    <p>Medicare for All EVERYWHERE is currently uninorporated, meaning we are a group of activists who meet regularly to organize actions and plan strategy but are not a registered LLC or 501(c) entity. Our goal is to become an official and registered coalition.</p>
-    <p>Medicare for All EVERYWHERE does not have a bank account and cannot take financial contributions. We are working to find a fiscal sponsor which will allow us to begin fundraising.</p>
+    <p>Medicare for All EVERYWHERE held its first meeting in October 2021 and has in its short time organized tweet storms, marches, and petition drives across multiple states and in Washington D.C. We have volunteer signups in more than 30 states and are working to build a nationwide mass movement for healthcare justice through grassroots organizing and direct action.</p>
+    <h2>Fiscal host</h2>
+    <p>Medicare for All EVERYWHERE has been fiscally hosted by Open Collective Foundation since November 2022, which gives us 501(c)3 nonprofit status without requiring us to manage a bank account, board of directors, or legal entity. A 5-8% share of donations go to Open Collective Foundation to handle our bookkeeping and compliance.</p>
+    <iframe src="https://www.youtube.com/embed/269SyUQmL_U" title="What is Open Collective Foundation? (2 minute animation)" style={{width:'100%', height:455, frameborder:0, scrolling:'no'}} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <h2>Financial transparency</h2>
+    <p>Medicare for All EVERYWHERE is committed to total financial transparency. All of our donations and reimbursements are done through <a href='https://opencollective.com/m4a-everywhere'>our Open Collective</a> where you can also view our budget which shows every contribution, expense, and payment. As a 501(c)3 we are generally prohibited from spending funds on political lobbying or campaign intervention, but all donations are tax-deductible in the United States.</p>
   </Layout>
 export default AboutPage
