416a737a712206c20168d1abd0896c8a928bf311
Andre Stackhouse <captainstack@gmail.com>
2022-12-30 15:22:39 -0800

    Update header/footer data in constants

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,5 +1,8 @@
 import TwitterLogo from '../images/twitter.svg'
+import MastodonLogo from '../images/mastodon.svg'
 import GitHubLogo from '../images/github.svg'
+import OpenCollectiveLogo from '../images/open-collective.svg'
+import SlackLogo from '../images/slack.svg'
 import EmailLogo from '../images/email.svg'
 
 export const HeaderLinks = [
@@ -68,6 +71,11 @@ export const HeaderLinks = [
     url: "https://actionnetwork.org/forms/medicare-for-all-everywhere-action-takers?source=direct_link&",
     class: "cta",
   },
+  {
+    text: "Donate",
+    url: "https://opencollective.com/m4a-everywhere/contribute/custom-donation-50029/checkout?interval=month&platformTip=0&amount=27",
+    class: "cta",
+  },
 ];
 
 export const AboutSidebarLinks = [
@@ -128,10 +136,28 @@ export const FooterSocialLinks = [
     title: 'Follow us on Twitter',
   },
   {
+    text: "Mastodon",
+    url: "https://twitter.com/m4a_everywhere",
+    logo: MastodonLogo,
+    title: 'Follow us on Mastodon',
+  },
+  {
     text: "GitHub",
     url: "https://github.com/Medicare-for-All-EVERYWHERE/main-site",
     logo: GitHubLogo,
     title: 'View source on GitHub',
+  },
+  {
+    text: "Open Collective",
+    url: "https://opencollective.com/m4a-everywhere",
+    logo: OpenCollectiveLogo,
+    title: 'Donate and view finances on Open Collective',
+  },
+  {
+    text: "Slack",
+    url: "https://actionnetwork.org/forms/code-of-conduct-2?source=direct_link&",
+    logo: SlackLogo,
+    title: 'Join our Slack',
   },
   {
     text: "Email",
