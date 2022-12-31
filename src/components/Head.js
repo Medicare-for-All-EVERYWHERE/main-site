dacec673631d7fd780b4f53c475b4a6c294e2723
Andre Stackhouse <captainstack@gmail.com>
2022-12-30 16:59:14 -0800

    Add contact form to contact page

--- src/components/Head.js
+++ src/components/Head.js
@@ -8,6 +8,8 @@ const Head = ({pageTitle = 'Page', siteTitle = 'Medicare for All EVERYWHERE'}) =
     <title>{pageTitle} | {siteTitle}</title>
     <meta content={Screenshot} property="og:image" />
     <meta property="og:description" content="Medicare for All EVERYWHERE is an aspiring coalition including every US state and territory united behind the goal of Medicare for All both statewide and federally." />
+    <link href='https://actionnetwork.org/css/style-embed-v3.css' rel='stylesheet' type='text/css' />
+    <script src='https://actionnetwork.org/widgets/v5/form/contact-medicare-for-all-everywhere?format=js&source=widget'></script>
   </Helmet>
 
 export default Head
--- src/pages/about/contact.js
+++ src/pages/about/contact.js
@@ -4,9 +4,10 @@ import { AboutSidebarLinks } from '../../helpers/constants'
 import '../../styles/styles.scss'
 
 const ContactPage = () =>
-  <Layout pageTitle="Contact" sidebarLinks={AboutSidebarLinks} activeHeaderLink="About">
+  <Layout pageTitle="Contact" activeHeaderLink="About">
     <h1>Contact</h1>
     <p>You can contact Medicare for All EVERYWHERE via email at <a href="mailto:massmovement.healthcare@gmail.com">massmovement.healthcare@gmail.com</a>.</p>
     <p>You may also join our Slack community to connect with our leadership and volunteers across the country. To get a Slack invite please view and agree to our <strong><a href='https://actionnetwork.org/forms/code-of-conduct-2?source=direct_link&'>Code of Conduct.</a></strong></p>
+    <div id='can-form-area-contact-medicare-for-all-everywhere' style={{width: '100%'}}></div>
   </Layout>
 export default ContactPage
